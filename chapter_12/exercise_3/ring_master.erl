0d43cb554348969b7e76c840da9614163190b53d
Benjamin Falk <bn_falk@yahoo.com>
2014-11-29 22:41:49 -0600

    Putting down the chain

--- chapter_12/exercise_3/ring_master.erl
+++ chapter_12/exercise_3/ring_master.erl
@@ -1,5 +1,6 @@
 -module(ring_master).
 -export([
+  build_chain/1,
   chain_talker/0,
   chain_talker/1,
   build/0,
@@ -16,6 +17,14 @@
   %First = chain_talker(),
   %spawn_next(1, WorkerCount, []).
 
+build_chain(Size) ->
+  build_chain(1, Size, build(), []).
+
+build_chain(Size, Size, P, L) -> [build(P)|L];
+build_chain(N, Size, P, L) -> 
+  Process = build(P),
+  build_chain(N+1, Size, Process, [Process|L]).
+
 build() ->
   spawn(ring_master, chain_talker, []).
 
@@ -25,7 +34,8 @@ build(Process) ->
 chain_talker() ->
   receive
     {link, Process} -> chain_talker(Process);
-    Any -> io:format("Error, not linked: ~p",[Any])
+    {msg, Message} -> io:format("Got Message: ~p~n", [Message]),
+                      chain_talker()
   end.
 
 chain_talker(Process) ->
