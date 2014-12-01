eed3b73743c7f28d61b3e01e939038b2e3ae0530
Benjamin Falk <benjamin.falk@yahoo.com>
2014-11-30 21:26:52 -0600

    Finished it!

--- chapter_12/exercise_3/ring_master.erl
+++ chapter_12/exercise_3/ring_master.erl
@@ -4,7 +4,8 @@
   chain_talker/0,
   chain_talker/1,
   build/0,
-  build/1
+  build/1,
+  send_msg/1
 ]).
 
 %loop(WorkerCount) ->
@@ -18,7 +19,14 @@
   %spawn_next(1, WorkerCount, []).
 
 build_chain(Size) ->
-  build_chain(1, Size, build(), []).
+  io:format("Building chain of size ~p~n", [Size]),
+  Chain = build_chain(1, Size, build(self()), []),
+  [First|_] = Chain,
+  {Time,_} = timer:tc(ring_master, send_msg, [First]),
+  io:format("Chain complete, took ~p microseconds~n",[Time]),
+  [X ! die || X <- Chain],
+  ok.
+
 
 build_chain(Size, Size, P, L) -> [build(P)|L];
 build_chain(N, Size, P, L) -> 
@@ -35,12 +43,20 @@ chain_talker() ->
   receive
     {link, Process} -> chain_talker(Process);
     {msg, Message} -> io:format("Got Message: ~p~n", [Message]),
-                      chain_talker()
+                      chain_talker();
+    die            -> void
   end.
 
 chain_talker(Process) ->
   receive
     {msg, Message} -> Process ! {msg, Message},
                       io:format("Got Message: ~p~n",[Message]),
-                      chain_talker(Process)
+                      chain_talker(Process);
+    die            -> void
+  end.
+
+send_msg(Process) ->
+  Process ! {msg, "Test"},
+  receive
+    {msg, Message} -> Message
   end.
