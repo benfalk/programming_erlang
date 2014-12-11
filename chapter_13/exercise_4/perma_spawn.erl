ca7323082c83bf101ba13f77a889a467a7bf0402
Benjamin Falk <bn_falk@yahoo.com>
2014-12-10 19:05:56 -0600

    Beware, winter is coming

---
+++ chapter_13/exercise_4/perma_spawn.erl
@@ -0,0 +1,31 @@
+-module(perma_spawn).
+-export([keep_alive_on/2]).
+
+keep_alive_on(Atom, Fun) ->
+  Pid = spawn(Fun),
+  register(Atom, Pid),
+  on_exit(Pid, fun()-> keep_alive_on(Atom, Fun) end),
+  heartbeat(Atom, Pid).
+
+on_exit(Pid, Fun) ->
+  spawn(fun()->
+    Ref = monitor(process, Pid),
+    receive
+      {'DOWN', Ref, process, Pid, Why} -> Fun(Why)
+    end
+  end).
+
+heartbeat(Atom, Pid) ->
+  spawn(fun()->
+    Ref = monitor(process, Pid),
+    heartbeat(Atom, Ref, Pid)
+  end).
+
+heartbeat(Atom, Ref, Pid) ->
+  io:format("Process is alive: ~p~n", [Atom]),
+  receive
+    {'DOWN', Ref, process, Pid, Why} -> io:format("Process died: ~p [~p]~n", [Atom, Why])
+    after 5000 -> heartbeat(Atom, Ref, Pid)
+  end.
+    
+    
