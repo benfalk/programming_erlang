bf4d0f1fa439bc25c3a479d741647b0e62b3f63b
Benjamin Falk <bn_falk@yahoo.com>
2014-12-11 13:20:22 -0600

    Fixing error with respawner

--- chapter_13/exercise_4/perma_spawn.erl
+++ chapter_13/exercise_4/perma_spawn.erl
@@ -11,7 +11,7 @@ on_exit(Pid, Fun) ->
   spawn(fun()->
     Ref = monitor(process, Pid),
     receive
-      {'DOWN', Ref, process, Pid, Why} -> Fun(Why)
+      {'DOWN', Ref, process, Pid, _} -> Fun()
     end
   end).
 
